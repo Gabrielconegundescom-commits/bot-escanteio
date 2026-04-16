824168b149b18da24be105c0b2af4044a7688b83
Gabrielconegundescom-commits <gabriel.conegundes.com@gmail.com>
2026-04-16 08:04:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new TelegramBot("SEU_TOKEN_AQUI", { polling: true });
+const bot = new TelegramBot("8653218083:AAH2kgAtpRinlaocNCpNF17XT9QkK5FskZc", { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "🔥 Bot de escanteio ATIVO!");
